f6b35e5f0c3d05e8e68f83aa9d9f6d86d4c7b229
Herbaltea <adam@quitenice.ca>
2020-10-31 18:11:24 -0400

    Initial Commit. But also first version of the page.

---
+++ web/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+  @use '@/assets/styles/bulma/bulma.sass';
+</style>
---
+++ web/src/views/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <section class="section">
+    <!-- Title -->
+    <section class="section">
+      <div class="container">
+        <div class="level">
+          <div class="level-left">
+            <div class="level-item">
+              <div class="container">
+                <h1 class="title is-1">Force Application Server</h1>
+                <h2 class="subtitile is-2">{{ currentTime }}</h2>
+              </div>
+            </div>
+          </div>
+          <div class="level-right">
+            <img src="@/assets/itrc_emblem.png" alt="" class="level-item">
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Subsections -->
+    <section class="section">
+      <div class="container">
+        <h4 class="subtitle is-4 is-spaced">Force</h4>
+        <div class="level">
+          <div class="level-left">
+            <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/Ef1AWlxP/Force.svg" alt="Force-Status-SVG" class="level-item">
+            <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/OFSz4FEm/FAS.svg" alt="FAS-Status-SVG" class="level-item" />
+            <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/6wCBBWGE/FAS-Services.svg" alt="FAS-Services-Status-SVG" class="level-item">
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Book -->
+    <section class="section">
+      <div class="container">
+        <h4 class="subtitle is-4 is-spaced">Book</h4>
+        <div class="level">
+          <div class="level-left">
+            <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/7hEVgVGf/Book.svg" alt="Book-Status-SVG" class="level-item" />
+            <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/9J1HMA0I/Book-API.svg" alt="Book-API-Status-SVG" class="level-item">
+            <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/-kdMrgLt/Book-Q.svg" alt="Book-Q-Status-SVG" class="level-item">
+            <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/Nplrc9-o/Book-Web.svg" alt="Book-Web-Status-SVG" class="level-item">
+          </div>
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import { format } from 'date-fns'
+export default {
+  name: 'Home',
+  components: {
+    
+  },
+  computed: {
+
+  },
+  data () {
+    return {
+      currentTime: '🤔',
+    }
+  },
+
+  mounted () {
+    setInterval(() => {
+      this.currentTime = format(new Date(), 'EEEE, MMMM dd pp ')
+    }, 1000)
+  }
+
+}
+</script>
