8a86bbec6dd7f1d5901458fa6f847a1889d7ecfa
Herbaltea <adam@quitenice.ca>
2020-10-31 18:54:53 -0400

    Updated docker to right ports. Updated .gitignore.

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,4 +4,17 @@
 
 <style lang="scss">
   @use '@/assets/styles/bulma/bulma.sass';
+
+  // Budget dark-mode for those who ask for it :P
+  @media (prefers-color-scheme: dark) {
+    $--marble: #fafafa;
+    $--darkness: #333333;
+    html, body {
+      color: $--marble;
+      background: $--darkness;
+    }
+    .title, .subtitle {
+      color: $--marble;
+    }
+  }
 </style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -7,8 +7,8 @@
           <div class="level-left">
             <div class="level-item">
               <div class="container">
-                <h1 class="title is-1">Force Application Server</h1>
-                <h2 class="subtitile is-2">{{ currentTime }}</h2>
+                <h1 class="title is-1">Force Status</h1>
+                <p class="">{{ currentTime }}</p>
               </div>
             </div>
           </div>
@@ -18,15 +18,26 @@
         </div>
       </div>
     </section>
-    <!-- Subsections -->
+    <!-- Force -->
     <section class="section">
       <div class="container">
-        <h4 class="subtitle is-4 is-spaced">Force</h4>
+        <h4 class="subtitle is-4">Force</h4>
         <div class="level">
           <div class="level-left">
             <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/Ef1AWlxP/Force.svg" alt="Force-Status-SVG" class="level-item">
-            <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/OFSz4FEm/FAS.svg" alt="FAS-Status-SVG" class="level-item" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Force Application Server -->
+    <section class="section">
+      <div class="container">
+        <h4 class="subtitle is-4">Force Application Server (FAS)</h4>
+        <div class="level">
+          <div class="level-left">
+            <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/OFSz4FEm/FAS.svg" alt="FAS-Status-SVG" class="level-item">
             <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/6wCBBWGE/FAS-Services.svg" alt="FAS-Services-Status-SVG" class="level-item">
+            <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/xP4gC36Z/FAS-Landing.svg" alt="FAS-Landiong-Status-SVG" class="level-item">
           </div>
         </div>
       </div>
@@ -34,7 +45,7 @@
     <!-- Book -->
     <section class="section">
       <div class="container">
-        <h4 class="subtitle is-4 is-spaced">Book</h4>
+        <h4 class="subtitle is-4">Book</h4>
         <div class="level">
           <div class="level-left">
             <img src="https://healthchecks.io/badge/d8c9adaf-5310-4bc8-a2c8-ade0ee/7hEVgVGf/Book.svg" alt="Book-Status-SVG" class="level-item" />
@@ -66,9 +77,15 @@ export default {
   },
 
   mounted () {
+    // Updates the time
     setInterval(() => {
       this.currentTime = format(new Date(), 'EEEE, MMMM dd pp ')
     }, 1000)
+
+    // Refreshed the page every 60 seconds (reloading SVGs from healthchecks)
+    setTimeout(() => {
+      this.$router.go()
+    }, 60000)
   }
 
 }
