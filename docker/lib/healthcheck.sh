8babd4d74d71a81b54fb36e338325120a973e181
Herbaltea <adam@quitenice.ca>
2020-11-02 08:54:02 -0500

    Apparently adding cron to this image is more complicated than I thought. Cutting an initial release because it works.

--- docker/lib/healthcheck.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Adds Hourly Healthcheck to crontab
-echo \n0 1 *   *   *   curl -fsS -m 10 --retry 5 -o /dev/null $HCIO_URL >> /etc/crontab
--- docker/lib/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Establish Healthcheck
-./healthchech.sh
